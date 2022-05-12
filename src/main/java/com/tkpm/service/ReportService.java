8150202e29ffe85c9444e728d96bc55b3947a443
nhatdang2604 <nhatdang2604@gmail.com>
2022-05-12 11:14:45 +0700

    Finish ReportService

--- src/main/java/com/tkpm/service/ReportService.java
+++ src/main/java/com/tkpm/service/ReportService.java
@@ -1,5 +1,13 @@
 package com.tkpm.service;
 
+import java.time.Month;
+import java.time.Year;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.tkpm.entities.Flight;
+import com.tkpm.wrapper.FlightStatisticWrapper;
 import com.tkpm.wrapper.report.BaseReport;
 
 public enum ReportService {
@@ -11,18 +19,45 @@ public enum ReportService {
 		flightService = FlightService.INSTANCE;
 	}
 	
-	public BaseReport getReportByMonth(int month, int year) {
-		//TODO:
+	public BaseReport getReportByMonth(Month month, int year) {
+		
 		BaseReport report = null;
 		
+		//Filternig the flights
+		Set<Flight> flights = flightService.findAllFlights();
+		List<FlightStatisticWrapper> wrappers = flights
+				.stream()
+				.filter(flight -> 
+					flight.getDateTime().getMonth().equals(month) &&
+					flight.getDateTime().getYear() == year)
+				.map(flight -> new FlightStatisticWrapper(flight))
+				.collect(Collectors.toList());
+		
+		//Setup report
+		report = new BaseReport(wrappers);
+		report.setMonth(month)
+			.setYear(year);
 		
 		return report;
 	}
 	
 	public BaseReport getReportByYear(int year) {
-		//TODO:
+		
 		BaseReport report = null;
 		
+		//Filternig the flights
+		Set<Flight> flights = flightService.findAllFlights();
+		List<FlightStatisticWrapper> wrappers = flights
+				.stream()
+				.filter(flight -> flight.getDateTime().getYear() == year)
+				.map(flight -> new FlightStatisticWrapper(flight))
+				.collect(Collectors.toList());
+		
+		//Setup report
+		report = new BaseReport(wrappers);
+		report.setYear(year);
+		
 		return report;
+		
 	}
 }

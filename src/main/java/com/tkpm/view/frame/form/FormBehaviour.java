0b763b0b277eeeae10993b11d11500fb1a9506cf
nhatdang2604 <nhatdang2604@gmail.com>
2022-05-27 17:01:05 +0700

    Add LoginForm

--- src/main/java/com/tkpm/view/frame/form/FormBehaviour.java
+++ src/main/java/com/tkpm/view/frame/form/FormBehaviour.java
@@ -7,7 +7,6 @@ public interface FormBehaviour {
 	public Object submit();
 	public JButton getSubmitButton();
 	public FormBehaviour setError(int errorCode);
-	public boolean isValidForm();
 	public void clear();
 	public void close();
 }

db0d275cb45cef933a07d8711b8f8403ffd78818
nhatdang2604 <nhatdang2604@gmail.com>
2022-06-02 00:05:10 +0700

    Add ManagerMainFrame to show up Manager's frame to open after loginning sucessfully

--- src/main/java/com/tkpm/view/frame/ManagerMainFrame.java
+++ src/main/java/com/tkpm/view/frame/ManagerMainFrame.java
@@ -3,21 +3,26 @@ package com.tkpm.view.frame;
 import com.tkpm.view.feature_view.BaseFeatureView;
 import com.tkpm.view.widget.Category;
 
-public class CustomerMainFrame extends BaseMainFrame {
+public class ManagerMainFrame extends BaseMainFrame {
 
 	//public static final int CUSTOMER_FEATURE_INDEX = 0;
 	
 	protected BaseFeatureView featureView;
+	protected BaseFeatureView featureView0;
 	
 	@Override
 	protected void initValueForComponents() {
 		
 		featureView = new BaseFeatureView();
+		
 		this.categories.add(new Category("Danh sách chuyến bay"));
-		this.featureViews.add(featureView);	
+		this.categories.add(new Category("Quản lý chuyến bay"));
+		
+		this.featureViews.add(featureView);
+		this.featureViews.add(featureView0);
 	}
 	
-	public CustomerMainFrame() {
+	public ManagerMainFrame() {
 		super();
 		
 	}

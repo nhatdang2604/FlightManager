986f9f9c26f1fcaf1ad504d8f20a099af616d471
nhatdang2604 <nhatdang2604@gmail.com>
2022-06-07 22:14:18 +0700

    Add ReportByYearView

--- src/main/java/com/tkpm/view/feature_view/report/ReportByYearView.java
+++ src/main/java/com/tkpm/view/feature_view/report/ReportByYearView.java
@@ -1,5 +1,44 @@
 package com.tkpm.view.feature_view.report;
 
-public class ReportByYearView {
+import java.awt.BorderLayout;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.tkpm.view.feature_view.table.ReportByYearTableView;
+
+public class ReportByYearView extends JPanel{
+
+	private Integer year;
+	private JLabel titleLabel;
+	private ReportByYearTableView table;
+	
+	private void initComponents() {
+		titleLabel = new JLabel("Báo cáo doanh thu năm");
+		table = new ReportByYearTableView();
+	}
+	
+	private void setLayout() {
+		setLayout(new BorderLayout());
+		add(titleLabel, BorderLayout.NORTH);
+		add(table, BorderLayout.CENTER);
+	}
+	
+	public ReportByYearView() {
+		initComponents();
+		setLayout();
+	}
+	
+	public void updateTitle() {
+		String title = "Báo cáo doanh thu năm" + (null != year?" " + year:"");
+		titleLabel.setText(title);
+		this.validate();
+	}
+	
+	public void setYear(Integer year) {
+		this.year = year;
+		updateTitle();
+	}
+	
+	public ReportByYearTableView getTable() {return table;}
 }

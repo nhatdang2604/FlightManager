5a1f144bca5be2ffbef60db9157ab01959f38ab9
nhatdang2604 <nhatdang2604@gmail.com>
2022-06-07 23:25:05 +0700

    Finish mock report by month feature to app

--- src/main/java/com/tkpm/view/feature_view/tabbed_controller_view/ReportTabbedControllerView.java
+++ src/main/java/com/tkpm/view/feature_view/tabbed_controller_view/ReportTabbedControllerView.java
@@ -5,8 +5,11 @@ import java.util.List;
 import javax.swing.JPanel;
 
 import com.tkpm.view.action.ChangeTabListener;
+import com.tkpm.view.feature_view.detail_view.ReportByMonthDetailView;
 import com.tkpm.view.feature_view.detail_view.ReportByYearDetailView;
+import com.tkpm.view.feature_view.header_view.ReportByMonthHeader;
 import com.tkpm.view.feature_view.header_view.ReportByYearHeader;
+import com.tkpm.view.feature_view.table.ReportByMonthTable;
 import com.tkpm.view.feature_view.table.ReportByYearTable;
 
 
@@ -14,12 +17,15 @@ public class ReportTabbedControllerView extends BaseTabbedControllerView {
 
 	//Header
 	private ReportByYearHeader reportByYearHeader;
+	private ReportByMonthHeader reportByMonthHeader;
 	
 	//Tables
 	private ReportByYearTable reportByYearTable;
-
+	private ReportByMonthTable reportByMonthTable;
+	
 	//Detail views
 	private ReportByYearDetailView reportByYearDetailView;
+	private ReportByMonthDetailView reportByMonthDetailView;
 	
 	public ReportTabbedControllerView(List<JPanel> backgroundParts) {
 		super(backgroundParts);
@@ -29,10 +35,20 @@ public class ReportTabbedControllerView extends BaseTabbedControllerView {
 		reportByYearTable = new ReportByYearTable();
 		reportByYearDetailView = new ReportByYearDetailView();
 		
-		//Add screen for user CRUD tab
+		//Init for report by month feature
+		reportByMonthHeader = new ReportByMonthHeader();
+		reportByMonthTable = new ReportByMonthTable();
+		reportByMonthDetailView = new ReportByMonthDetailView();
+		
+		//Add screen for report by month tab
 		this.addCenterAsTable(reportByYearTable, "Báo cáo theo năm");
 		this.addDetail(reportByYearDetailView);
 		this.addHeader(reportByYearHeader);
+		
+		//Add screen for report by year tab
+		this.addCenterAsTable(reportByMonthTable, "Báo cáo theo tháng");
+		this.addDetail(reportByMonthDetailView);
+		this.addHeader(reportByMonthHeader);
 		
 		//Change detail panel and header panel while change tab
 		tabbedPanel.addMouseListener(new ChangeTabListener(this, backgroundParts));
@@ -43,5 +59,9 @@ public class ReportTabbedControllerView extends BaseTabbedControllerView {
 	public ReportByYearTable getReportByYearTable() {return reportByYearTable;}
 	public ReportByYearHeader getReportByYearHeader() {return reportByYearHeader;}
 	public ReportByYearDetailView getReportByYearDetailView() {return reportByYearDetailView;}
+
+	public ReportByMonthHeader getReportByMonthHeader() {return reportByMonthHeader;}
+	public ReportByMonthTable getReportByMonthTable() {return reportByMonthTable;}
+	public ReportByMonthDetailView getReportByMonthDetailView() {return reportByMonthDetailView;}
 	
 }

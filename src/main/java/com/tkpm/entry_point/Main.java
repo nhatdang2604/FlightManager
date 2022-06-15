b727edb913efc7115f80e9687d5e7d42b66916a9
nhatdang2604 <nhatdang2604@gmail.com>
2022-06-15 20:35:07 +0700

    Decorata main

--- src/main/java/com/tkpm/entry_point/Main.java
+++ src/main/java/com/tkpm/entry_point/Main.java
@@ -5,35 +5,7 @@ import com.tkpm.controller.LoginController;
 public class Main {
 
 	public static void main(String[] args) {
-		//LoginForm logForm = new LoginForm();
-//		Login logForm = new Login();
-//		//RegistrationForm regForm = new RegistrationForm(logForm);
-//		Registration regForm = new Registration();
-//		logForm.getRegistrateButton().addActionListener(event -> {
-//			logForm.close();
-//			regForm.open();
-//		});
-//		regForm.getSubmitButton().addActionListener(event -> {
-//			logForm.open();
-//			regForm.close();
-//		});
-//		regForm.getCancelButton().addActionListener(event -> {
-//			logForm.open();
-//		});
-		//logForm.open();
-		//AirportTransitionForm airportTransitionForm = new AirportTransitionForm();
-		//airportTransitionForm.open();
-		
-		//BaseMainFrame mainFrame = new CustomerMainFrame();
-		//BaseMainFrame mainFrame = new ManagerMainFrame();
-//		BaseMainFrame mainFrame = new AdminMainFrame();
-//		mainFrame.open();
-		
-//		//User user = UserService.INSTANCE.getUsersWithReservations(Arrays.asList(8)).get(0);
-//		AdminController controller = new AdminController(new AdminMainFrame());
-//		//controller.setAccount(user.getAccount());
-//		controller.run();
-		
+
 		LoginController controller = new LoginController();
 		controller.run();
 		

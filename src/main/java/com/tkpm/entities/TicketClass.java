f2f2390df712c3a947fbd0fea139694cfd5effab
tanthokg <htho379@gmail.com>
2022-06-19 19:26:17 +0700

    Override hashCode method for TicketClass

--- src/main/java/com/tkpm/entities/TicketClass.java
+++ src/main/java/com/tkpm/entities/TicketClass.java
@@ -36,4 +36,13 @@ public class TicketClass {
 	//Setters
 	public void setId(Integer id) {this.id = id;}
 	public void setName(String name) {this.name = name;}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
 }

9d5624f670f32dc7e4924444ffb40392d575d74e
tanthokg <htho379@gmail.com>
2022-06-20 22:53:13 +0700

    Added create, update, delete method for AirportDAO

--- src/main/java/com/tkpm/dao/AirportDAO.java
+++ src/main/java/com/tkpm/dao/AirportDAO.java
@@ -24,6 +24,82 @@ public enum AirportDAO {
 		factory = HibernateUtil.INSTANCE.getSessionFactory();
 	}
 	
+	//Create new airport
+		public Airport create(Airport airport) {
+			
+			Session session = factory.getCurrentSession();
+			
+			try {
+				session.beginTransaction();
+				
+				//Save the airport to database
+				Integer id = (Integer) session.save(airport);
+				
+				//Update the current id for the given airport
+				airport.setId(id);
+				
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				session.getTransaction().rollback();
+			} finally {
+				session.getTransaction().commit();
+				session.close();
+			}
+			
+			return airport;
+		}
+		
+		//Update an airport
+		public Airport update(Airport airport) {
+			
+			Session session = factory.getCurrentSession();
+			
+			try {
+				session.beginTransaction();
+				
+				//Update the airport
+				session.update(airport);
+				
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				session.getTransaction().rollback();
+			} finally {
+				session.getTransaction().commit();
+				session.close();
+			}
+			
+			return airport;
+		}
+		
+		//Delete an airport by the given id
+		public int delete(Integer id) {
+
+			Session session = factory.getCurrentSession();
+			int errorCode = 0;
+			
+			try {
+				session.beginTransaction();
+				
+				Airport airport = session.get(Airport.class, id);
+				
+				if (null !=  airport) {
+					session.delete(airport);
+				}
+				
+			} catch (Exception ex) {
+				
+				ex.printStackTrace();
+				session.getTransaction().rollback();
+				errorCode = 1;
+			} finally {
+				session.getTransaction().commit();
+				session.close();
+			}
+		
+			return errorCode;
+		}
+		
+		
 
 	public Airport find(String name) {
 		Session session = factory.getCurrentSession();

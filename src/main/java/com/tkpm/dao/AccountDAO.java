3ad3ea45a0d83a912a42fe1be71e19ea262c70ef
tanthokg <htho379@gmail.com>
2022-06-20 22:48:12 +0700

    Added create method for Account DAO

--- src/main/java/com/tkpm/dao/AccountDAO.java
+++ src/main/java/com/tkpm/dao/AccountDAO.java
@@ -22,5 +22,31 @@ public enum AccountDAO {
 	private AccountDAO() {
 		factory = HibernateUtil.INSTANCE.getSessionFactory();
 	}
+	
+	//Create new account
+		public BaseAccount create(BaseAccount account) {
+			
+			Session session = factory.getCurrentSession();
+			
+			try {
+				session.beginTransaction();
+				
+				//Save the account to database
+				Integer id = (Integer) session.save(account);
+				
+				//Update the current id for the given account
+				account.setId(id);
+				
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				session.getTransaction().rollback();
+			} finally {
+				session.getTransaction().commit();
+				session.close();
+			}
+			
+			return account;
+		}
+		
 }
  

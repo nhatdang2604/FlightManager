5c579282f47e600913057f08bcd97503dd639d38
tanthokg <htho379@gmail.com>
2022-06-20 22:54:09 +0700

    Fixed indents

--- src/main/java/com/tkpm/dao/AirportDAO.java
+++ src/main/java/com/tkpm/dao/AirportDAO.java
@@ -25,126 +25,126 @@ public enum AirportDAO {
 	}
 	
 	//Create new airport
-		public Airport create(Airport airport) {
+	public Airport create(Airport airport) {
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			session.beginTransaction();
 			
-			Session session = factory.getCurrentSession();
+			//Save the airport to database
+			Integer id = (Integer) session.save(airport);
 			
-			try {
-				session.beginTransaction();
-				
-				//Save the airport to database
-				Integer id = (Integer) session.save(airport);
-				
-				//Update the current id for the given airport
-				airport.setId(id);
-				
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				session.getTransaction().rollback();
-			} finally {
-				session.getTransaction().commit();
-				session.close();
-			}
+			//Update the current id for the given airport
+			airport.setId(id);
 			
-			return airport;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			session.getTransaction().commit();
+			session.close();
 		}
 		
-		//Update an airport
-		public Airport update(Airport airport) {
+		return airport;
+	}
+	
+	//Update an airport
+	public Airport update(Airport airport) {
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			session.beginTransaction();
 			
-			Session session = factory.getCurrentSession();
+			//Update the airport
+			session.update(airport);
 			
-			try {
-				session.beginTransaction();
-				
-				//Update the airport
-				session.update(airport);
-				
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				session.getTransaction().rollback();
-			} finally {
-				session.getTransaction().commit();
-				session.close();
-			}
-			
-			return airport;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			session.getTransaction().commit();
+			session.close();
 		}
 		
-		//Delete an airport by the given id
-		public int delete(Integer id) {
+		return airport;
+	}
+	
+	//Delete an airport by the given id
+	public int delete(Integer id) {
 
-			Session session = factory.getCurrentSession();
-			int errorCode = 0;
-			
-			try {
-				session.beginTransaction();
-				
-				Airport airport = session.get(Airport.class, id);
-				
-				if (null !=  airport) {
-					session.delete(airport);
-				}
-				
-			} catch (Exception ex) {
-				
-				ex.printStackTrace();
-				session.getTransaction().rollback();
-				errorCode = 1;
-			} finally {
-				session.getTransaction().commit();
-				session.close();
-			}
+		Session session = factory.getCurrentSession();
+		int errorCode = 0;
 		
-			return errorCode;
+		try {
+			session.beginTransaction();
+			
+			Airport airport = session.get(Airport.class, id);
+			
+			if (null !=  airport) {
+				session.delete(airport);
+			}
+			
+		} catch (Exception ex) {
+			
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+			errorCode = 1;
+		} finally {
+			session.getTransaction().commit();
+			session.close();
+		}
+	
+		return errorCode;
+	}
+	
+	//Find all airports in database
+	public List<Airport> findAll() {
+		
+		Session session = factory.getCurrentSession();
+		List<Airport> airports = new ArrayList<>();
+		
+		try {
+			session.beginTransaction();
+			
+			//Get the list of airports
+			airports = session.createQuery("from " + Airport.class.getName(), Airport.class).getResultList();
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			session.getTransaction().commit();
+			session.close();
 		}
 		
-		//Find all airports in database
-				public List<Airport> findAll() {
-					
-					Session session = factory.getCurrentSession();
-					List<Airport> airports = new ArrayList<>();
-					
-					try {
-						session.beginTransaction();
-						
-						//Get the list of airports
-						airports = session.createQuery("from " + Airport.class.getName(), Airport.class).getResultList();
-						
-					} catch (Exception ex) {
-						ex.printStackTrace();
-						session.getTransaction().rollback();
-					} finally {
-						session.getTransaction().commit();
-						session.close();
-					}
-					
-					return airports;
-					
-				}
-				
-				//Find airport by id
-				public Airport find(Integer id) {
-					
-					Session session = factory.getCurrentSession();
-					Airport airport = null;
-					
-					try {
-						session.beginTransaction();
-						
-						airport = session.get(Airport.class, id);
-						
-					} catch (Exception ex) {
-						
-						ex.printStackTrace();
-						session.getTransaction().rollback();
-					} finally {
-						session.getTransaction().commit();
-						session.close();
-					}
-				
-					return airport;
-				}	
+		return airports;
+		
+	}
+	
+	//Find airport by id
+	public Airport find(Integer id) {
+		
+		Session session = factory.getCurrentSession();
+		Airport airport = null;
+		
+		try {
+			session.beginTransaction();
+			
+			airport = session.get(Airport.class, id);
+			
+		} catch (Exception ex) {
+			
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			session.getTransaction().commit();
+			session.close();
+		}
+	
+		return airport;
+	}	
 
 	public Airport find(String name) {
 		Session session = factory.getCurrentSession();

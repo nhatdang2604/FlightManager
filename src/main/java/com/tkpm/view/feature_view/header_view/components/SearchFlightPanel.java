f33f5587aee4bf463b54fbfe6d53f5463cf4d626
tanthokg <htho379@gmail.com>
2022-06-22 10:27:18 +0700

    Added ComboBox for search panel

--- src/main/java/com/tkpm/view/feature_view/header_view/components/SearchFlightPanel.java
+++ src/main/java/com/tkpm/view/feature_view/header_view/components/SearchFlightPanel.java
@@ -82,6 +82,13 @@ public class SearchFlightPanel extends JPanel{
 				for (int i = 0; i < size; ++i) {
 					panels.get(i).add(labels.get(i));
 				}
+				
+				//Add combo boxes
+				int offset = 0;
+				for (int i = 0; i < airportComboBoxes.size(); ++i) {
+					airportComboBoxes.get(i).setPreferredSize(datePickers.get(i).getPreferredSize());
+					panels.get(i).add(airportComboBoxes.get(i));
+				}
 		
 		
 	}
